fb9f3071cd13cadb3727a64c5987aeb56fc7687a
JHorger <Joshua_Horger@reyrey.com>
2022-10-31 10:59:16 -0500

    added comment model, controller, and partial views

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
     has_one_attached :image
+    has_many :comments
+
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
 end
